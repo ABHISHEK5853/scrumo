b58156eb8f2f286999d555a01ea27487d744dd00
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-08 23:11:37 +0530

    Add a component Library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { TestComponent } from "@/features/test"
 
 export default function Home() {
   return (
     <div>
-      <Button variant = "ghost">Click me</Button>
+      {/* <Button variant = "ghost">Click me</Button>
+      <p className="text-red-500 font-semibold">
+        Scrumo
+      </p> */}
+      <TestComponent></TestComponent>
     </div>
   )
 }
