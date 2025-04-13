cbeaa70404fc9bf6047b0ba6195134e16ca081b9
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-13 16:32:12 +0530

    feat(ui): add reusable dotted separator component for section breaks

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,19 +1,25 @@
 import SignInPage from "@/app/(auth)/sign-in/page";
+import { Separator } from "@/components/ui/separator";
 import{
     Card,
     CardContent,
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/ui/dotted-separator";
 
 export const SignInCard = () => {
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-            <CardHeader>
-                <CardTitle>
+            <CardHeader className="flex items-center justify-center text-center p-7">
+                <CardTitle className="text-2xl">
                     Welcome Back!
                 </CardTitle>
             </CardHeader>
+            <div className="px-7 mb-2">
+                <DottedSeparator />
+
+            </div>
         </Card>
     );
 
