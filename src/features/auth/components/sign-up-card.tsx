c6f4c71feaadec9fe24ce7b5d1b7b0966ff1cc10
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-13 21:40:23 +0530

    feat(auth): add new SignUpCard and restyle existing SignInCard for improved compact layout

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -2,20 +2,32 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { DottedSeparator } from "@/components/ui/dotted-separator";
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
   return (
     <Card className="w-full h-full md:w-[400px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-6">
-        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription className="text-sm mt-2">
+          By signing up, you agree to our{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Terms of Service</span>
+          </Link>
+        </CardDescription>
       </CardHeader>
 
       <div className="px-6">
