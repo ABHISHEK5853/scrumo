e46b0cffe5ac0ca593dfb750e782daaabf951545
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-15 23:24:15 +0530

    feat(auth): finalize sign-up flow with API integration
    - Created useRegister hook for sign-up logic
    - Updated SignUpCard component with new API flow

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -24,19 +24,16 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import { registerSchema } from "../schemas";
+import { useRegister } from "../api/use-register";
 
-const formSchema = z.object(
-    {
-        name: z.string().trim().min(1,"Name is required"),
-        email: z.string().email(),
-        password: z.string().min(8,"Minimum 8 characters required"),
-    }
-);
 
 export const SignUpCard = () => {
 
-        const form = useForm<z.infer<typeof formSchema>>({
-            resolver: zodResolver(formSchema),
+  const {mutate} = useRegister();
+
+        const form = useForm<z.infer<typeof registerSchema>>({
+            resolver: zodResolver(registerSchema),
             defaultValues: {
                 name:"",
                 email: "",
@@ -44,8 +41,9 @@ export const SignUpCard = () => {
             },
         });
     
-        const onSubmit = (values: z.infer<typeof formSchema>) => {
+        const onSubmit = (values: z.infer<typeof registerSchema>) => {
             console.log({values});
+            mutate({json:values});
         };
 
   return (
