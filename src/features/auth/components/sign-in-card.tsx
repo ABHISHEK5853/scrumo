55966a905f2c844d2a45f9b2c5e35e8f73e385cb
Abhishek Thakur <abhishekthakurn0@gmail.com>
2025-04-15 23:12:53 +0530

    feat(auth): add sign-up API, RPC setup, and improved type safety
    - Implemented sign-up flow and related API routes
    - Added RPC utility for client-server communication
    - Improved type safety across auth-related code
    - Refactored SignInCard component

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -24,25 +24,22 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 
-// for logging in
-const formSchema = z.object(
-    {
-        email: z.string().email(),
-        password: z.string().min(1,"Required"),
-    }
-);
+import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const {mutate} = useLogin();
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
         defaultValues: {
             email: "",
             password: "",
         },
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log({values});
+    const onSubmit = (values: z.infer<typeof loginSchema>) => {
+        // console.log({values});
+        mutate({ json:values });
     };
     return (
         <Card className="w-full h-full md:w-[400px] border-none shadow-none">
